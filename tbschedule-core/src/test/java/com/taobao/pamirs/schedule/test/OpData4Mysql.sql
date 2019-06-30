399becf89f78d654471b17d9dd69d92f1a7290ad
胡群 <hzhuqun@corp.netease.com>
2019-06-30 22:17:37 +0800

    修改日志相关jar包版本

--- tbschedule-core/src/test/java/com/taobao/pamirs/schedule/test/OpData4Mysql.sql
+++ tbschedule-core/src/test/java/com/taobao/pamirs/schedule/test/OpData4Mysql.sql
@@ -2,3 +2,4 @@ select count(ID) from SCHEDULE_TEST where OWN_SIGN = 'BASE'  and mod(ID,10) in (
 select  mod(23,10);
 update SCHEDULE_TEST set OWN_SIGN = 'BASE';
 update SCHEDULE_TEST set STS ='N';
+update SCHEDULE_TEST set DEAL_COUNT =1;
